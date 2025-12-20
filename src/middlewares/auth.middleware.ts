7d0d62ba60b6e7294aa6bd25f949615087d126d0
Muhammed Ajmal <ajmalkunjammed@gmail.com>
2025-12-20 11:14:39 +0530

    fixing bugs on middlware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,21 +1,60 @@
 import { Request, Response, NextFunction } from "express";
-
-import { verifyAccessToken } from "../utils/jwt";
+import jwt from "jsonwebtoken";
 import { AppError } from "../utils/AppError";
 
-export const requireAuth = (req:Request, _res:Response, next:NextFunction) => {
-  const token = req.cookies.accessToken;
+export const requireAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const accessToken = req.cookies.accessToken;
+  const refreshToken = req.cookies.refreshToken;
 
-  if (!token) {
-    return next(new AppError("Unauthorized", 401));
+  
+  if (!accessToken && !refreshToken) {
+    throw new AppError("Unauthorized", 401);
   }
 
-  try {
-    const payload = verifyAccessToken(token);
-    req.userId = payload.userId;
-    next();
-  } catch {
-    next(new AppError("Unauthorized", 401));
+
+  if (accessToken) {
+    try {
+      const payload = jwt.verify(
+        accessToken,
+        process.env.ACCESS_SECRET!
+      ) as any;
+
+      req.userId = payload.userId;
+      return next();
+    } catch (err) {
+      
+    }
   }
+
+  if (refreshToken) {
+    try {
+      const payload = jwt.verify(
+        refreshToken,
+        process.env.REFRESH_SECRET!
+      ) as any;
+
+      
+      const newAccessToken = jwt.sign(
+        { userId: payload.userId },
+        process.env.ACCESS_SECRET!,
+        { expiresIn: "15m" }
+      );
+
+      res.cookie("accessToken", newAccessToken, {
+        httpOnly: true,
+        sameSite: "strict",
+      });
+
+      req.userId = payload.userId;
+      return next();
+    } catch {
+      throw new AppError("Unauthorized", 401);
+    }
+  }
+
+  throw new AppError("Unauthorized", 401);
 };
-
